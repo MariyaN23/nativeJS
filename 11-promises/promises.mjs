9c79cb585caf5ce636cd5f21dec3b332c6fccb6d
MariyaN23 <sellaite505@gmail.com>
2023-12-10 17:57:23 +0300

    11 promises

--- 11-promises/promises.mjs
+++ 11-promises/promises.mjs
@@ -1,4 +1,62 @@
-import axios from 'axios';
+const axios = {
+    _fake(url, data) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let responseData = {
+                    text: `${url} loves you`
+                }
+                if (url.indexOf('it-kamasutra') > 0) {
+                    responseData = {
+                        requestedCount: data.count,
+                        message: 'we will prepare students for you'
+                    }
+                } else if (url.indexOf('google') > 0) {
+                    responseData = {
+                        vacancies: 12
+                    }
+                } else if (url.indexOf('microsoft') > 0) {
+                    responseData = {
+                        vacancies: 21
+                    }
+                }
+                resolve({
+                    request: {},
+                    status: 200,
+                    headers: {},
+                    config: {},
+                    data: responseData
+                })
+            }, randomIntFromInterval(1, 5)*1000)
+        })
+    },
+    post(url, data) {
+        return this._fake(url, data)
+    },
+    get(url, data) {
+        return this._fake(url, data)
+    }
+}
+
+const findUserInDB = (id) => {
+    const users = [
+        {id: 1, name: 'Dimych', friend: 3},
+        {id: 2, name: 'Valera', friend: null},
+        {id: 3, name: 'Oleg', friend: 2}
+    ]
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            let user = users.find(u => u.id === id)
+            if (user === null) {
+                rej('user not found')
+            } else {
+                res(user)
+            }
+        }, randomIntFromInterval(500, 1500))
+    })
+}
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random()*(max-min + 1)+min)
+}
 
 const makeGoogleRequest =()=> {
     const pr = axios.get('https://www.google.com')
@@ -158,3 +216,67 @@ const run3 = async ()=> {
 }
 
 run3()
+
+const catchPromise = findUserInDB(1)
+    .then(user => {
+        console.log(user.name)
+        return user.friend
+    })
+    .then(friendID=>findUserInDB(friendID))
+    .catch((error) => {
+        return {name: "fake bot", friend: 2}
+    })
+    .then(friend1 => {
+        console.log(friend1.name)
+        return friend1.friend
+    })
+    .then(friendID => findUserInDB(friendID))
+    .then(friend2 => {
+        console.log(friend2.name)
+        return friend2
+    })
+    .catch((error) => alert(error))
+
+let run = async () => {
+    try {
+        let user = await findUserInDB(1)
+        console.log(user)
+        let friend1
+        try {
+            friend1 = await findUserInDB(user.friend)
+        } catch (error) {
+            friend1 = {name: "fake bot", friend: 2}
+        }
+        console.log(friend1)
+        let friend2 = await findUserInDB(friend1.friend)
+        console.log(friend2)
+    } catch (error) {
+        alert(error)
+    }
+}
+
+run()
+
+const api = {
+    save() {
+
+    },
+    read() {
+        return Promise.resolve({name: 'it-kamasutra'})
+    }
+}
+async function run() {
+    await api.save()
+    console.log('saved')
+    let data = await api.read()
+    console.log(data)
+}
+run()
+
+async function xxx() {
+}
+async function run2() {
+    let a = xxx()
+    console.log(a)
+}
+run2()

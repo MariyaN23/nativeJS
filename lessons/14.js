fbaab5728a9d258523f59988e1f6c57680a05918
MariyaN23 <sellaite505@gmail.com>
2024-02-09 21:31:05 +0300

    14

--- lessons/14.js
+++ lessons/14.js
@@ -89,7 +89,7 @@ let car5 = car4
 car4 = null
 car5.showMaxSpeed() //this - 200*/
 
-const car6 = {
+/*const car6 = {
     maxSpeed: 200
 }
 const car7 = {
@@ -101,9 +101,67 @@ function carSpeed() {
 car6.showMaxSpeed = carSpeed
 car7.showMaxSpeed = carSpeed
 car6.showMaxSpeed() //200
-car7.showMaxSpeed() //300
+car7.showMaxSpeed() //300*/
 
 //3. call, apply, bind
+/*const scooter = {
+    maxSpeed: 20
+}
 
+const car = {
+    maxSpeed: 300,
+    showMaxSpeed(a, b) {
+        console.log(this.maxSpeed + a + b)
+    }
+}
 
-//4. Function constuctor
+car.showMaxSpeed.call(scooter, 20, 30)
+const arr = [10, 20]
+car.showMaxSpeed.apply(scooter, arr)
+
+car.showMaxSpeed.bind(scooter, 70, 50)()
+
+const scooter2 = {
+    maxSpeed: 20
+}
+const car2 = {
+    maxSpeed: 300,
+    showMaxSpeed(a, b, c, d) {
+        console.log(this.maxSpeed + a + b)
+        console.log(c, d)
+    }
+}
+car2.showMaxSpeed.bind(scooter2, 10, 20)(30, 40)*/
+
+/*const scooter = {
+    maxSpeed: 20
+}
+const bus = {
+    maxSpeed: 40
+}
+const car = {
+    maxSpeed: 300,
+    showMaxSpeed() {
+        console.log(this.maxSpeed)
+    }
+}
+car.showMaxSpeed.bind(scooter).call(bus)*/
+
+/*const ferrari = {
+    maxSpeed: 333
+}
+
+var maxSpeed = 100
+const car = {
+    maxSpeed: 300,
+    showMaxSpeed() {
+        console.log(this.maxSpeed)
+    },
+    showMaxSpeed2: ()=> {
+        console.log(this.maxSpeed)
+    }
+}
+car.showMaxSpeed2() //global - undefined, window - 100
+car.showMaxSpeed2.call(ferrari)*/
+
+//4. Function constructor

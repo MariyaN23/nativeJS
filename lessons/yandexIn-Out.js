70bedfcfc09c6db721a5da2bb94780f698a72d94
MariyaN23 <sellaite505@gmail.com>
2024-03-30 18:02:33 +0300

    input/output node.js

---
+++ lessons/yandexIn-Out.js
@@ -0,0 +1,25 @@
+//terminal
+/*sum = (data) => {
+    let mas = data.toString().split(' ');
+    return +mas[0] + +mas[1];
+};
+
+let cnt, res;
+process.stdin.on('data', data => {
+    res = sum(data);
+    process.stdout.write(res + '');
+    process.exit();
+});*/
+
+
+//input.txt && output.txt
+/*
+const fs = require('fs')
+
+let fileContent = fs.readFileSync("input.txt", "utf8");
+
+const [a, b] = fileContent.toString().split(' ')
+
+const result = Number(a) + Number(b)
+
+fs.writeFileSync("output.txt", result.toString())*/
